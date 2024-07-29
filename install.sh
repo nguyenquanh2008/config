ebc9836e9a0645a17f4288f7ded53e88c3b888f0
Quanh <86000827+nguyenquanh2008@users.noreply.github.com>
2024-07-29 11:57:53 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,5 @@
+apt install bc bison build-essential lib32ncurses5-dev libncurses5 libncurses5-dev ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev -y
+wget http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.4-2_amd64.deb && sudo dpkg -i libtinfo5_6.4-2_amd64.deb && rm -f libtinfo5_6.4-2_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.4-2_amd64.deb && sudo dpkg -i libncurses5_6.4-2_amd64.deb && rm -f libncurses5_6.4-2_amd64.deb
+git clone -b lineage-17.1 https://github.com/LineageOS/android_kernel_samsung_universal9810
+git clone https://github.com/nguyenquanh2008/toolchain

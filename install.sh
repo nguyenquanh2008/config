2f129478130af5c4c79c828ffa15ff799c874692
Quanh <nguyendanhquocanh25082008@gmail.com>
2024-08-12 23:08:03 +0700

    new

--- install.sh
+++ install.sh
@@ -1,5 +1,24 @@
-apt install bc bison build-essential lib32ncurses5-dev libncurses5 libncurses5-dev ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev -y
-wget http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.4-2_amd64.deb && sudo dpkg -i libtinfo5_6.4-2_amd64.deb && rm -f libtinfo5_6.4-2_amd64.deb
-wget http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.4-2_amd64.deb && sudo dpkg -i libncurses5_6.4-2_amd64.deb && rm -f libncurses5_6.4-2_amd64.deb
-git clone -b lineage-17.1 https://github.com/LineageOS/android_kernel_samsung_universal9810
-git clone https://github.com/nguyenquanh2008/toolchain
+#!/bin/sh
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
+wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+mkdir ~/ccminer
+cd ~/ccminer
+GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/Oink70/Android-Mining/releases?per_page=1" | jq -c '[.[] | del (.body)]')
+GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .browser_download_url")
+GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .name")
+
+echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
+
+wget ${GITHUB_DOWNLOAD_URL} -O ~/ccminer/ccminer
+wget https://raw.githubusercontent.com/lukewrightmain/VerusCliMining/main/config.json -O ~/ccminer/config.json
+chmod +x ~/ccminer/ccminer
+
+cat << EOF > ~/ccminer/start.sh
+#!/bin/sh
+~/ccminer/ccminer -c ~/ccminer/config.json
+EOF
+chmod +x start.sh
